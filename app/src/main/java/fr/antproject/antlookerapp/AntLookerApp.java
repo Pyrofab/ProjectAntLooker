6b96411ef3d652e15e11ea22d1cc8c07ef6ce3c2
FourmiPanda <ismail.el-khantache@etu.univ-nantes.fr>
2018-02-22 18:58:41 +0100

    Ajout de pouvoir prendre une photo ou d'en sélectionné une de la Gallery.

--- app/src/main/java/fr/antproject/antlookerapp/AntLookerApp.java
+++ app/src/main/java/fr/antproject/antlookerapp/AntLookerApp.java
@@ -1,11 +1,17 @@
 package fr.antproject.antlookerapp;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import org.opencv.core.Mat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AntLookerApp extends AppCompatActivity {
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int PICK_IMAGE = 2;
 
     static {
         System.loadLibrary("opencv_java3");
@@ -15,7 +21,64 @@ public class AntLookerApp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mat m = new Mat();
         setContentView(R.layout.activity_ant_looker_app);
+
+        Button takePicture = (Button) findViewById(R.id.takePicture);
+        takePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        Button selectPicture = (Button) findViewById(R.id.importPicture);
+        selectPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectPhotoFromGallery();
+            }
+        });
+
+
+
+    }
+
+
+    private void selectPhotoFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                if( data != null){
+                    Log.d("Take picture",data.toString());
+                    TextView t = (TextView) findViewById(R.id.textView);
+                    t.setText(data.toString());
+                }
+
+            }
+        } else if(requestCode == PICK_IMAGE){
+            if (resultCode == RESULT_OK) {
+                if( data != null){
+                    Log.d("Take picture",data.toString());
+                    TextView t = (TextView) findViewById(R.id.textView);
+                    t.setText(data.toString());
+                }
+
+            }
+        }
     }
 }
+
