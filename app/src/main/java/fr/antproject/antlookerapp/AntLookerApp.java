a796a515bef90c2413df5ba49589c0f08c62d05b
FourmiPanda <ismail.el-khantache@etu.univ-nantes.fr>
2018-03-01 15:44:50 +0100

    J'enleve les imports inutiles

--- app/src/main/java/fr/antproject/antlookerapp/AntLookerApp.java
+++ app/src/main/java/fr/antproject/antlookerapp/AntLookerApp.java
@@ -1,12 +1,7 @@
 package fr.antproject.antlookerapp;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -14,7 +9,6 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
